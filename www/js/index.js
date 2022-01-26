3b8329779a9a8f24299ddb726941577bbcd9333c
CallumGWilliams <57756266+CallumGWilliams@users.noreply.github.com>
2022-01-26 14:52:20 +0000

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -19,11 +19,118 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+
+function getDate(){
+	let d = new Date();
+	let m = d.getMonth() + 1;
+
+	if (m < 10){
+		m = "0" + m;
+	}
+	console.log(m);
+	return d.getFullYear() + "-" + (m) + "-" + (d.getDate());
+
+}
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
+let sports = [];
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
 }
+
+$("#footballBtn").click(function(){
+
+//hide sports
+// add back button in the top corner
+//display football nav or something - showing games
+// do this for every sport
+
+})
+
+
+
+
+
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://sportscore1.p.rapidapi.com/sports",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "sportscore1.p.rapidapi.com",
+		"x-rapidapi-key": "3e74b52bfbmshfa3bed0081d07cdp101d2djsn5b7bbb4fdc8a"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+
+
+	for (let i = 0; i < response.data.length; i++){
+	sports.push(response.data[i].name);
+	}
+
+
+console.log(sports.length);
+
+for (let u = 0; u < sports.length; u++){
+
+
+
+
+
+
+let btn = document.createElement("button");
+btn.innerHTML = sports[u];
+btn.id = sports[u];
+
+$("#sportP").append(sports[u] + "<br>");
+}
+	
+
+
+});
+
+const getByDate = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://livescore6.p.rapidapi.com/matches/v2/list-by-date?Category=soccer&Date=" + getDate(),
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "livescore6.p.rapidapi.com",
+		"x-rapidapi-key": "3e74b52bfbmshfa3bed0081d07cdp101d2djsn5b7bbb4fdc8a"
+	}
+};
+
+$.ajax(getByDate).done(function (response) {
+
+	let upcomingGames = [];
+	console.log(response);
+	for (let i = 0; i < response.Stages.length; i++){
+		if (response.data[i].status === "notstarted"){
+
+			let game = {sport:response.data[i].sport.name,
+				home:response.data[i].home_team.name,
+				away:response.data[i].away_team.name,
+				date:response.data[i].start_at}
+
+				upcomingGames.push(game);
+		}
+	}
+
+	console.log(upcomingGames);
+});
+
+function getNextEvents(){
+
+	let d = new Date();
+	let date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+	let time = d.getHours() - 7 + ":" + d.getMinutes() + ":" + d.getSeconds();
+	return date + " " + time;
+
+
+
+}
+
+
