2b7c2551df0e19c3189b107981a5a44fb56b3baa
CallumGWilliams <57756266+CallumGWilliams@users.noreply.github.com>
2022-02-21 19:06:01 +0000

    account creation added

--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,252 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+let accountsDb = new PouchDB("accounts");
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+
+
+
+async function getDocs(){
+let promise = new Promise((resolve,reject) => {
+
+accountsDb.allDocs({
+            include_docs: true,
+            attachments: true
+        }).then(function (result) {
+resolve(result);
+
+}).catch(function (err){
+reject (err);})
+
+})
+
+let result = await promise;
+return result;
+};
+
+async function login(){
+
+getDocs().then(value => {
+console.log(value);
+let c = 0;
+
+for (i=0;i < value.rows.length; i++){
+
+
+console.log($("#loginEmail").val() + $("#loginPass").val());
+if ($("#loginEmail").val() === value.rows[i].doc.email && $("#loginPass").val() === value.rows[i].doc.pass){
+console.log(("test"));
+let ac = value.rows[i].doc;
+
+console.log("MATCH AT ENTRY: " + i);
+$("#logRes").text("Logging in now!");
+setTimeout(loginAccount, 3000, ac);
+c = 1;
+
+
+}
+if (c === 0){
+$("#logRes").text("Incorrect Username or Password!");
+}}
+
+
+});
+
+};
+
+function createUser(){
+
+document.getElementById("userLogin").style.display = "none";
+document.getElementById("userCreate").style.display = "block";
+
+}
+
+async function register(){
+
+let uniqueEmail = 0;
+
+let u = $("#username").val();
+let e = $("#email").val();
+let d = $("#dob").val();
+let p = $("#pass").val();
+
+
+
+ getDocs().then(result => {
+
+        for (i = 0; i < result.rows.length; i++){
+        if (e === result.rows[i].doc.email){
+
+    console.log("EMAIL ALREADY IN USE!");
+
+    $("#createRes").text("Email already in use - please use another email or log in!");
+    uniqueEmail = 1;
+    console.log(uniqueEmail);
+        }
+        }
+
+if (uniqueEmail === 0){
+
+if (u !== "" && e !== "" && d !== "" && p !== ""){
+
+let id = result.rows.length +1;
+let acc = {
+
+_id:id.toString(),
+username:u,
+email:e,
+dob:d,
+pass:p
+
+}
+
+console.log(acc);
+
+
+
+accountsDb.put(acc);
+$("#createRes").text("Creation Successful - Logging in now!");
+setTimeout(loginAccount,  3000, acc);
+}
+else $("#createRes").text("Please enter a value into all fields!")
+}
+
+        });
+
+
+}
+
+
+$(document).ready(function(){
+
+
+/*
+$("#createAccountBtn").click(function(){
+let uniqueEmail = 0;
+
+let u = $("#username").val();
+let e = $("#email").val();
+let d = $("#dob").val();
+let p = $("#pass").val();
+
+
+
+  accountsDb.allDocs({
+            include_docs: true,
+            attachments: true
+        }).then(function (result) {
+        console.log(result);
+        console.log(result.rows.length);
+
+        for (i = 0; i < result.rows.length; i++){
+        if (e === result.rows[i].doc.email){
+
+    console.log("EMAIL ALREADY IN USE!");
+
+    $("#createRes").text("Email already in use - please use another email or log in!");
+    uniqueEmail = 1;
+    console.log(uniqueEmail);
+        }
+        }
+
+if (uniqueEmail === 0){
+
+if (u !== "" && e !== "" && d !== "" && p !== ""){
+
+let id = result.rows.length +1;
+let acc = {
+
+_id:id.toString(),
+username:u,
+email:e,
+dob:d,
+pass:p
+
+}
+
+console.log(acc);
+
+
+
+accountsDb.put(acc);
+setTimeout(loginAccount,  3000, acc);
+}
+else $("#createRes").text("Please enter a value into all fields!")
+}
+
+        });
+
+
+
+$("#createRes").text("ACCOUNT CREATED SUCCESSFULLY - WELCOME, " + u);
+
+
+
+
+
+})
+
+
+
+$("#losRes").text("result");
+*/
+
+
+
+$("#loginBtn").on('click touch', function(){
+//check login!!!
+
+$("#createRes").text("Mobile is trying");
+login();
+
+})
+
+
+
+
+
+$("#venueBtn").click(function(){
+
+document.getElementById("userCreate").style.display = "none";
+   document.getElementById("venueAccount").style.display = "block";
+   console.log("hi");
+
+})
+
+$("#userBtn").click(function(){
+   document.getElementById("venueAccount").style.display = "none";
+document.getElementById("userCreate").style.display = "block";
+
+
+
+})
+
+});
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+
+
+
+let sports = [];
+
+
+};
+
+
+
+$("#createBtn").click(function(){
+
+document.getElementById("userLogin").style.display = "none";
+document.getElementById("userCreate").style.display = "block";
+
+
+
+})
+
+
+
+
 
 function getDate(){
 	let d = new Date();
@@ -32,11 +260,28 @@ function getDate(){
 
 }
 
-document.addEventListener('deviceready', onDeviceReady, false);
+function loginAccount(user) {
 
-let sports = [];
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+uLogin = document.getElementById("userLogin");
+uCreate = document.getElementById("userCreate");
+
+if (uLogin.style.display !== "none"){
+uLogin.style.display = "none";
+}
+
+if(uCreate.style.display !== "none"){
+uCreate.style.display ="none";}
+
+
+document.getElementById("loggedIn").style.display = "block";
+
+$("#loggedInRes").text("Welcome Back, " + user.username);
+
+}
+function toggle(id){
+
+console.log(id);
+
 }
 
 $("#footballBtn").click(function(){
@@ -108,15 +353,15 @@ $.ajax(getByDate).done(function (response) {
 	let upcomingGames = [];
 	console.log(response);
 	for (let i = 0; i < response.Stages.length; i++){
-		if (response.data[i].status === "notstarted"){
+		//if (response.data[i].status === "notstarted"){
 
-			let game = {sport:response.data[i].sport.name,
-				home:response.data[i].home_team.name,
-				away:response.data[i].away_team.name,
-				date:response.data[i].start_at}
+		//	let game = {sport:response.data[i].sport.name,
+			//	home:response.data[i].home_team.name,
+			//	away:response.data[i].away_team.name,
+			//	date:response.data[i].start_at}
 
-				upcomingGames.push(game);
-		}
+			//	upcomingGames.push(game);
+		//}
 	}
 
 	console.log(upcomingGames);
@@ -132,5 +377,7 @@ function getNextEvents(){
 
 
 }
+
+
 
 
