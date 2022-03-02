ea52dd7c78fb23d777b555407240b464634c1fec
CallumGWilliams <57756266+CallumGWilliams@users.noreply.github.com>
2022-03-02 16:55:39 +0000

    added venues display

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,7 @@
 let accountsDb = new PouchDB("accounts");
 let venuesDb = new PouchDB("venues");
+//let remoteAccounts = new PouchDB("http://localhost:8000/remoteAccounts");
+//let remoteVenues = new PouchDB("http://localhost:8000/remoteVenues");
 var map;
 var lat;
 var lng;
@@ -13,7 +15,6 @@ function getLocation(){
 
 if (navigator.geolocation){
 navigator.geolocation.getCurrentPosition(showPosition);
-console.log("exec");
 } else {
 console.log("Geolocation is not supported by this browser.");
 }
@@ -24,7 +25,6 @@ function showPosition(position){
 
 lat = position.coords.latitude,
 lng = position.coords.longitude
-console.log("exec 2");
 initMap();
 
 }
@@ -37,9 +37,6 @@ initMap();
 function initMap(){
 
 
-
-console.log(lat);
-console.log(lng);
 
 map = new google.maps.Map(document.getElementById("map"), {
 zoom: 10,
@@ -67,8 +64,7 @@ function placeMarker(loc) {
     });
 
   }
-      console.log(loc.lat());
-      console.log(loc.lng());
+
 
       venLat = loc.lat();
       venLng = loc.lng();
@@ -113,9 +109,6 @@ document.getElementById("venPic").style.display = "block";
 
 
 
-console.log(n);
-
-
 let location = {
 lat: venLat,
 lng: venLng,
@@ -157,9 +150,7 @@ latLng:location
 }
 
 venuesDb.put(venue);
-console.log(venue);
 
-console.log(getVenues());
 $("#venueCreateRes").text("Venue created successfully!");
 
 }
@@ -202,18 +193,18 @@ return result;
 async function login(){
 
 getDocs().then(value => {
-console.log(value);
+
 let c = 0;
 
 for (i=0;i < value.rows.length; i++){
 
 
-console.log($("#loginEmail").val() + $("#loginPass").val());
+
 if ($("#loginEmail").val() === value.rows[i].doc.email && $("#loginPass").val() === value.rows[i].doc.pass){
-console.log(("test"));
+
 let ac = value.rows[i].doc;
 
-console.log("MATCH AT ENTRY: " + i);
+
 $("#logRes").text("Logging in now!");
 setTimeout(loginAccount, 3000, ac);
 c = 1;
@@ -252,11 +243,9 @@ let p = $("#pass").val();
         for (i = 0; i < result.rows.length; i++){
         if (e === result.rows[i].doc.email){
 
-    console.log("EMAIL ALREADY IN USE!");
-
     $("#createRes").text("Email already in use - please use another email or log in!");
     uniqueEmail = 1;
-    console.log(uniqueEmail);
+
         }
         }
 
@@ -275,7 +264,7 @@ pass:p
 
 }
 
-console.log(acc);
+
 
 
 
@@ -287,90 +276,18 @@ else $("#createRes").text("Please enter a value into all fields!")
 }
 
         });
-
-
 }
 
 
 $(document).ready(function(){
 
 
-/*
-$("#createAccountBtn").click(function(){
-let uniqueEmail = 0;
 
-let u = $("#username").val();
-let e = $("#email").val();
-let d = $("#dob").val();
-let p = $("#pass").val();
-
-
-
-  accountsDb.allDocs({
-            include_docs: true,
-            attachments: true
-        }).then(function (result) {
-        console.log(result);
-        console.log(result.rows.length);
-
-        for (i = 0; i < result.rows.length; i++){
-        if (e === result.rows[i].doc.email){
-
-    console.log("EMAIL ALREADY IN USE!");
-
-    $("#createRes").text("Email already in use - please use another email or log in!");
-    uniqueEmail = 1;
-    console.log(uniqueEmail);
-        }
-        }
-
-if (uniqueEmail === 0){
-
-if (u !== "" && e !== "" && d !== "" && p !== ""){
-
-let id = result.rows.length +1;
-let acc = {
-
-_id:id.toString(),
-username:u,
-email:e,
-dob:d,
-pass:p
-
-}
-
-console.log(acc);
-
-
-
-accountsDb.put(acc);
-setTimeout(loginAccount,  3000, acc);
-}
-else $("#createRes").text("Please enter a value into all fields!")
-}
-
-        });
-
-
-
-$("#createRes").text("ACCOUNT CREATED SUCCESSFULLY - WELCOME, " + u);
-
-
-
-
-
-})
-
-
-
-$("#losRes").text("result");
-*/
 
 $("#venueBtn").click(function(){
 
 document.getElementById("userCreate").style.display = "none";
    document.getElementById("venueAccount").style.display = "block";
-   console.log("hi");
 
 })
 
@@ -386,7 +303,7 @@ document.getElementById("userCreate").style.display = "block";
 
 function createVenue(){
 
-console.log("Heyo");
+
 document.getElementById("createVenue").style.display = "block";
 
 }
@@ -399,15 +316,42 @@ function onDeviceReady() {
 getLocation();
 
 
-//map = new google.maps.Map(document.getElementById('map'), {
-//center:
-//})
+
+if ($("body").is(".onVenues")){
+renderVenues();
+}
 
 
 
 
 
 };
+
+async function renderVenues(){
+
+getVenues().then(value => {
+
+for (i = 0; i < value.rows.length;i++){
+
+let $div = $("<div>" , {id: "div" + value.rows[i].id, class:"ven"});
+let $p = $("<p></p>", {id: "p" + value.rows[i].id}).text(value.rows[i].doc.name + " , " + value.rows[i].doc.postcode);
+let $i = $("<img></img>", {id:"i" + value.rows[i].id, src:value.rows[i].doc.image, class:"venImg"});
+
+
+
+$div.append($p);
+$div.append($i);
+
+$("#venues").append($div);
+
+}
+
+
+
+});
+
+
+}
 
 
 
@@ -431,7 +375,6 @@ function getDate(){
 	if (m < 10){
 		m = "0" + m;
 	}
-	console.log(m);
 	return d.getFullYear() + "-" + (m) + "-" + (d.getDate());
 
 }
@@ -452,11 +395,6 @@ uCreate.style.display ="none";}
 document.getElementById("loggedIn").style.display = "block";
 
 $("#loggedInRes").text("Welcome Back, " + user.username);
-
-}
-function toggle(id){
-
-console.log(id);
 
 }
 
